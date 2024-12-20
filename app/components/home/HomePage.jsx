3f8d2605cca374f8e400033024873583a2a13a45
JoeTheBest <jewelshahi10@gmail.com>
2024-12-21 00:27:51 +0200

    AOS added

--- app/components/home/HomePage.jsx
+++ app/components/home/HomePage.jsx
@@ -2,6 +2,8 @@
 import { useState, useEffect } from "react";
 import "./home.css";
 import Image from "next/image";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const HomePage = () => {
   const [mobileVersion, setVersion] = useState(false);
@@ -25,6 +27,7 @@ const HomePage = () => {
 
   useEffect(() => {
     checkRes();
+    AOS.init(); // Initialize AOS animations
 
     // Add event listener for window resize
     window.addEventListener("resize", checkRes);
@@ -37,6 +40,7 @@ const HomePage = () => {
 
   return (
     <div>
+      {/* Parallax background (no AOS animation) */}
       <div
         className="landingPage"
         style={{
@@ -49,38 +53,61 @@ const HomePage = () => {
         }}
       >
         <div className="main-ca">
-          <div className="ca-info">
+          <div className="ca-info" data-aos="fade-up" data-aos-delay="0">
             <Image
               src="/images/logo-tus-white.png"
               alt="Description"
               width={293}
               height={70}
               className="img-logo"
+              data-aos="fade-up"
+              data-aos-delay="200"
             />
-            <h1>Explore Our Engineering!</h1>
-            <a href="#">See Our Work</a>
+            <h1 data-aos="fade-up" data-aos-delay="500">
+              Explore Our Engineering!
+            </h1>
+            <a href="#" data-aos="fade-up" data-aos-delay="700">
+              See Our Work
+            </a>
           </div>
         </div>
       </div>
+
+      {/* Text and Images with AOS animation */}
       <div className="home-join-us">
         <div className="formula-student-info">
-          <h5>WELCOME TO TUS RACING TEAM</h5>
-          <h1>Formula Student in Sofia</h1>
-          <div className="sl"></div>
-          <p>
+          <h5 data-aos="fade-up" data-aos-delay="200">
+            WELCOME TO TUS RACING TEAM
+          </h5>
+          <h1 data-aos="fade-up" data-aos-delay="250">
+            Formula Student in Sofia
+          </h1>
+          <div className="sl" data-aos="fade-up" data-aos-delay="250"></div>
+          <p data-aos="fade-up" data-aos-delay="300">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem ut
             commodi delectus aspernatur, doloribus quam veritatis repellendus.
             Similique sed, non eligendi reiciendis necessitatibus, et animi,
             blanditiis at debitis pariatur temporibus.
           </p>
-          <a href="/formula-student">More about formula student</a>
+          <a href="/formula-student" data-aos="fade-up" data-aos-delay="300">
+            More about formula student
+          </a>
         </div>
-        <div className="looking">
+
+        <div className="looking" data-aos="fade-up" data-aos-delay="350">
           <h1>We are searching for new talent in the following areas:</h1>
           <div className="sl"></div>
 
-          <div className={`looking-teams ${mobileVersion ? "teams-res" : ""}`}>
-            <div className={`team-desc ${mobileVersion ? "team-res" : ""}`}>
+          <div
+            className={`looking-teams ${mobileVersion ? "teams-res" : ""}`}
+            data-aos="fade-down"
+            data-aos-delay="450"
+          >
+            <div
+              className={`team-desc ${mobileVersion ? "team-res" : ""}`}
+              data-aos="fade-right"
+              data-aos-delay="550"
+            >
               <h3
                 onClick={() => {
                   openTeamInfo("Mechanical");
@@ -97,7 +124,11 @@ const HomePage = () => {
                 </ul>
               )}
             </div>
-            <div className={`team-desc ${mobileVersion ? "team-res" : ""}`}>
+            <div
+              className={`team-desc ${mobileVersion ? "team-res" : ""}`}
+              data-aos="fade-down"
+              data-aos-delay="650"
+            >
               <h3
                 onClick={() => {
                   openTeamInfo("Electrical");
@@ -114,13 +145,17 @@ const HomePage = () => {
                 </ul>
               )}
             </div>
-            <div className={`team-desc ${mobileVersion ? "team-res" : ""}`}>
+            <div
+              className={`team-desc ${mobileVersion ? "team-res" : ""}`}
+              data-aos="fade-up"
+              data-aos-delay="750"
+            >
               <h3
                 onClick={() => {
                   openTeamInfo("Media");
                 }}
               >
-                {mobileVersion ? "Media >" : "Media"}
+                {(!mobileVersion || openTeam == "Media") && "Media"}
               </h3>
               {(!mobileVersion || openTeam == "Media") && (
                 <ul>
@@ -131,7 +166,11 @@ const HomePage = () => {
                 </ul>
               )}
             </div>
-            <div className={`team-desc ${mobileVersion ? "team-res" : ""}`}>
+            <div
+              className={`team-desc ${mobileVersion ? "team-res" : ""}`}
+              data-aos="fade-left"
+              data-aos-delay="850"
+            >
               <h3
                 onClick={() => {
                   openTeamInfo("Business");
@@ -155,11 +194,15 @@ const HomePage = () => {
               e.preventDefault();
               sendMessage();
             }}
+            data-aos="zoom-in"
+            data-aos-delay="550"
+            className="go-to-application"
           >
             GO TO THE APPLICATION
           </a>
         </div>
       </div>
+
       <div
         className="sub-section"
         style={{
@@ -170,12 +213,13 @@ const HomePage = () => {
         }}
       ></div>
 
+      {/* Team Organization Section */}
       <div className="team-organization">
-        <div className="team-information">
-          <h1>Apply now!</h1>
-          <div className="sl"></div>
+        <div className="team-information" data-aos="fade-up" data-aos-delay="200">
+          <h1 data-aos="fade-right" data-aos-delay="400">Apply now!</h1>
+          <div className="sl" data-aos="fade-down" data-aos-delay="500"></div>
           <br />
-          <p>
+          <p data-aos="fade-left" data-aos-delay="700">
             Do you fancy motorsport and tinkering? <br />
             Do you have team spirit and want to get to know your fellow
             students? <br />
@@ -186,14 +230,20 @@ const HomePage = () => {
             <br />
             Simply apply here!
           </p>
-          <a href="/apply">GO TO THE APPLICATION</a>
+          <a href="/apply" data-aos="fade-up" data-aos-delay="500">
+            GO TO THE APPLICATION
+          </a>
         </div>
+
+        {/* Image with animation */}
         <Image
           src="/images/team-photos/team-photo.jpg"
           alt="Description"
           width={700}
           height={500}
           className="img-team"
+          data-aos="zoom-in"
+          data-aos-delay="900"
         />
       </div>
     </div>
